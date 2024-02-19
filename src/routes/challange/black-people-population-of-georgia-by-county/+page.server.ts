ec9411477161bbf4dcccc26e8602bceb14a90c2b
Akaki Mikaia <akakimikaia@gmail.com>
2024-02-19 10:24:41 +0000

    backstage: simplify geojson

---
+++ src/routes/challange/black-people-population-of-georgia-by-county/+page.server.ts
@@ -0,0 +1,7 @@
+import geoJson from '../../../data/challenge01/geojson.json';
+
+export async function load() {
+	return {
+		geoJson
+	};
+}
--- src/routes/challange/negro-population-of-georgia-by-county/+page.server.ts
+++
@@ -1,7 +0,0 @@
-export async function load({url}) {
-  const res = await fetch(`${url.origin}/challenge01/geojson.json`)
-  const geoJson = await res.json()
-	return {
-		geoJson
-	};
-}
